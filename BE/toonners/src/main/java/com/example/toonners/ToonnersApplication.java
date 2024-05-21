c21c8e272d4a32208d1901e6e9111fbbd1a01046
chanee <hanssang6301@gmail.com>
2024-05-22 02:14:14 +0900

    [BE] feat: innit commit

---
+++ BE/toonners/src/main/java/com/example/toonners/ToonnersApplication.java
@@ -0,0 +1,13 @@
+package com.example.toonners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonnersApplication.class, args);
+	}
+
+}
