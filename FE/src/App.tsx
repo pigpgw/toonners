ba9cff115b9df1d406299b69ffed8007333244b9
pigpgw <ceh20002@naver.com>
2024-05-22 12:50:41 +0900

    [FE] feat: 추천글 생성 라우팅 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,6 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.scss";
 import Login from "./pages/Login";
+import NewRecomand from "./routes/NewRecomand";
+import NewChatRoom from "./routes/NewChatRoom";
 import Signup from "./routes/Signup";
 
 function App() {
@@ -9,19 +11,22 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup/*" element={<Signup />} />
+
         <Route path="/home/chatroom" element={<div>홈 단톡방</div>} />
         <Route path="/home/recommend" element={<div>홈 추천글</div>} />
+
         <Route path="/chatroom/:id" element={<div>단톡방 상세</div>} />
         <Route path="/chatroomlist" element={<div>단톡방 리스트</div>} />
-        <Route path="/chatroom/new/*" element={<div>단톡방 만들기</div>} />
+
+        <Route path="/chatroom/new/*" element={<NewChatRoom />} />
         <Route path="/chatroom/search" element={<div>단톡방 검색</div>} />
-        
+
         <Route path="/recommend/:id" element={<div>추천글 상세</div>} />
-        <Route path="/recommend/new/*" element={<div>추천글 만들기</div>} />
+        <Route path="/recommend/new/*" element={<NewRecomand />} />
 
         <Route path="/mypage" element={<div>마이페이지</div>} />
         <Route path="/profile/:userId" element={<div>상대방 프로필</div>} />
-        <Route path="/*" element={<div>잘못된 경로</div>} />
+        <Route path="*" element={<div>잘못된 경로</div>} />
       </Routes>
     </>
   );
