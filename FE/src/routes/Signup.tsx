6c8f8bd9a61a22f8d1b017da702a265ebe6d9ab1
pigpgw <ceh20002@naver.com>
2024-05-22 11:36:17 +0900

    feat: singup redirect 설정

--- FE/src/routes/Signup.tsx
+++ FE/src/routes/Signup.tsx
@@ -1,7 +1,8 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const Signup = () => (
   <Routes>
+    <Route path="/" element={<Navigate to="1" />} />
     <Route path="1" element={<div>사용자 등록1</div>} />
     <Route path="2" element={<div>사용자 등록2</div>} />
     <Route path="3" element={<div>사용자 등록3</div>} />
