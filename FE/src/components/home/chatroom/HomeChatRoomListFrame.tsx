5b50d02be66b48ac9dfb319e55f31af2df938a50
taboowiths <tabou9820@gmail.com>
2024-05-23 20:34:10 +0900

    [FE] feat: 메인 컴포넌트 중 공통 부분 분리

--- FE/src/components/home/chatroom/HomeChatRoomListFrame.tsx
+++ FE/src/components/home/chatroom/HomeChatRoomListFrame.tsx
@@ -2,15 +2,19 @@ import { ReactNode } from "react";
 import styles from "@styles/home/Home.module.scss";
 import Text from "@components/common/Text";
 import Arrow from "@/components/common/Arrow";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   title: string;
   subtitle: string;
   component: ReactNode;
+  isMore: boolean;
+  more: string;
   onClick?: () => void;
 }
 
-const ChatRoomList = ({ title, subtitle, component }: Props) => {
+const HomeChatListFrame = ({ title, subtitle, component, isMore, more }: Props) => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className={styles.chatroom__title}>
@@ -18,7 +22,7 @@ const ChatRoomList = ({ title, subtitle, component }: Props) => {
           <Text types="sub-header" bold="semi-bold">
             {title}
           </Text>
-          <Arrow onClick={() => console.log("더보기")} />
+          {isMore && <Arrow onClick={() => navigate(more)} />}
         </div>
         <Text types="body-1">{subtitle}</Text>
       </div>
@@ -27,4 +31,4 @@ const ChatRoomList = ({ title, subtitle, component }: Props) => {
   );
 };
 
-export default ChatRoomList;
+export default HomeChatListFrame;
