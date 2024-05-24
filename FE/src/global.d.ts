e751b4301853b53dc97b7f7e947f53c2b7f9855e
taboowiths <tabou9820@gmail.com>
2024-05-24 14:59:47 +0900

    [FE] chore: svgr 라이브러리 설치 및 설정 추가

--- FE/src/global.d.ts
+++ FE/src/global.d.ts
@@ -2,3 +2,8 @@ declare module "*.scss" {
   const content: { [className: string]: string };
   export = content;
 }
+
+declare module "*.svg" {
+  const value: React.FC<React.SVGProps<SVGElement>>;
+  export default value;
+}
