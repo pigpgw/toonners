11f2f944e0a768c5670a550526f3dc5a7f28236d
taboowiths <tabou9820@gmail.com>
2024-05-25 04:06:55 +0900

    [FE] feat: 웹툰 정보 검색 API 연결

--- FE/src/components/home/chatroom/create/CreateChatRoom1.tsx
+++ FE/src/components/home/chatroom/create/CreateChatRoom1.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "@styles/home/ChatRoom.module.scss";
 import Header from "@components/common/Header";
@@ -6,10 +6,35 @@ import Text from "@/components/common/Text";
 import Input from "@/components/common/Input";
 import SearchedWebtoonCard from "@/components/Webtoon/SearchedWebtoonCard";
 import Modal from "@/components/common/Modal";
+import { WebtoonConfig } from "@/interface/Webtoon.interface";
+import { useChatActions } from "@/slices/chatSlice";
+import fetchWetboonInfo from "@/api/fetchWetboonInfo";
 
 const CreateChatRoom1 = () => {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  const [webtoonList, setWebtoonList] = useState<WebtoonConfig[]>([]);
+  const { setSelected } = useChatActions();
+
+  const setSelectedWebtoon = (webtoon: WebtoonConfig) => {
+    setSelected(webtoon);
+    navigate("/chatroom/create/2");
+  };
+
+  useEffect(() => {
+    const getWebtoonList = async () => {
+      try {
+        const res = await fetchWetboonInfo(keyword);
+        setWebtoonList(res);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    if (keyword.length > 0) getWebtoonList();
+    setWebtoonList([]);
+  }, [keyword]);
 
   const handleBack = () => {
     navigate("/home");
@@ -24,12 +49,24 @@ const CreateChatRoom1 = () => {
             어떤 웹툰에 대해 이야기할까요?
           </Text>
         </div>
-        <Input types="search" placeholder="웹툰 제목을 입력하세요." />
+        <Input
+          types="search"
+          placeholder="웹툰 제목, 웹툰 작가를 입력하세요."
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
         <div className={styles.webtoon}>
-          <SearchedWebtoonCard title="웹툰 제목" imgUrl="" clicked={false} />
-          <SearchedWebtoonCard title="웹툰 제목" imgUrl="" clicked={false} />
-          <SearchedWebtoonCard title="웹툰 제목" imgUrl="" clicked={false} />
-          <SearchedWebtoonCard title="웹툰 제목" imgUrl="" clicked={false} />
+          {webtoonList.map((webtoon, i) => {
+            return (
+              <SearchedWebtoonCard
+                key={i}
+                title={webtoon.title}
+                imgUrl={webtoon.img}
+                clicked={false}
+                onClick={() => setSelectedWebtoon(webtoon)}
+              />
+            );
+          })}
         </div>
       </div>
       {open && (
