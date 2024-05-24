196c39c19963fce67d71a32e1cde3a18516b9fc8
taboowiths <tabou9820@gmail.com>
2024-05-24 18:48:26 +0900

    [FE] fix: svg 파일 경로 에러 수정

--- FE/src/global.d.ts
+++ FE/src/global.d.ts
@@ -2,8 +2,3 @@ declare module "*.scss" {
   const content: { [className: string]: string };
   export = content;
 }
-
-declare module "*.svg" {
-  const value: React.FC<React.SVGProps<SVGElement>>;
-  export default value;
-}
