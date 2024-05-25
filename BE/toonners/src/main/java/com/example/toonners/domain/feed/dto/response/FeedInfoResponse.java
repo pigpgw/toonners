1b823d5ff7d0ea7ed4ffaffc4fca86d61cfd2b80
chanee <hanssang6301@gmail.com>
2024-05-26 04:37:44 +0900

    [BE] fix: 누락된 피드 반환 값 추가
    - 작성자 닉네임 피드 반환 값에 추가

--- BE/toonners/src/main/java/com/example/toonners/domain/feed/dto/response/FeedInfoResponse.java
+++ BE/toonners/src/main/java/com/example/toonners/domain/feed/dto/response/FeedInfoResponse.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 public class FeedInfoResponse {
     private Long parentFeedId;
     private Long writerMemberId;
+    private String writerMemberNickname;
     private String writerMemberImage;
     private String feedTitle;
     private String feedContexts;
@@ -33,6 +34,7 @@ public class FeedInfoResponse {
         return FeedInfoResponse.builder()
                 .parentFeedId(feed.getId())
                 .writerMemberId(feed.getWriter().getId())
+                .writerMemberNickname(feed.getWriter().getNickname())
                 .writerMemberImage(feed.getWriter().getImage())
                 .feedTitle(feed.getTitle())
                 .feedContexts(feed.getContexts())
