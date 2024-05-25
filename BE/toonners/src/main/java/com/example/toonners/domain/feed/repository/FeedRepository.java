7661ea62cd83d3761386c0ee5a23803f634a9001
hanchanhee <hanssang6301@gamil.com>
2024-05-25 19:56:50 +0900

    [BE] feat & docs: 피드 전체 조회 기능 추가, 피드 생성 설명 문서 작성
    - 부모 피드 (안의 추천 웹툰을 가지고 있는 게시글) 전체 조회 기능
    - 피드 생성 설명 문서 작성.

--- BE/toonners/src/main/java/com/example/toonners/domain/feed/repository/FeedRepository.java
+++ BE/toonners/src/main/java/com/example/toonners/domain/feed/repository/FeedRepository.java
@@ -4,6 +4,9 @@ import com.example.toonners.domain.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    List<Feed> findAllByParentFlag(Boolean flag);
 }
