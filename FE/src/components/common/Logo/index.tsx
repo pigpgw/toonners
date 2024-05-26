88285aa37b3ec9523320daa318e507d2f1a6a1d9
taboowiths <tabou9820@gmail.com>
2024-05-26 18:05:32 +0900

    [FE] feat: 로고 클릭 시 메인페이지로 이동

--- FE/src/components/common/Logo/index.tsx
+++ FE/src/components/common/Logo/index.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import styles from "@styles/common/Logo.module.scss";
 import LogoImg from "@assets/images/home/Logo.svg?react";
 
 const Logo = () => {
+  const navigate = useNavigate();
   return (
-    <div className={styles.logo}>
+    <div className={styles.logo} onClick={() => navigate("/home")}>
       <LogoImg />
     </div>
   );
