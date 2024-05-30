b18aab6cafb7246263ca456e721626d3ccba235b
chanee <hanssang6301@gmail.com>
2024-05-30 22:39:31 +0900

    [BE] feat: Like entity 생성
    - id, member, feed

--- BE/toonners/src/main/java/com/example/toonners/domain/fire/entity/Fire.java
+++ BE/toonners/src/main/java/com/example/toonners/domain/fire/entity/Fire.java
@@ -18,7 +18,7 @@ public class Fire extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "FIRE_ID")
 
-    private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     @OnDelete(action = OnDeleteAction.CASCADE)
