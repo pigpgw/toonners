f75473ebf99d9c2b1d84e9d0f7b2ab9c532d3735
pigpgw <ceh20002@naver.com>
2024-06-20 01:25:50 +0900

    [FE] feat: 콤마가 들어간 웹툰도 필터링

--- FE/src/api/fetchWetboonInfo.ts
+++ FE/src/api/fetchWetboonInfo.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const fetchWebtoonInfo = async (title: string): Promise<UserWebtoonListConfig[]> => {
   const response = await axios.get(`https://korea-webtoon-api-cc7dda2f0d77.herokuapp.com/webtoons?keyword=${title}`);
   const webtoonData = response.data.webtoons || [];
-  const invalidPattern = /[;[\]]/;
+  const invalidPattern = /[;[\],]/;
   const filteredWebtoons = webtoonData
     .filter((webtoon: WebtoonResponseConfig) => !invalidPattern.test(webtoon.title))
     .map((webtoon: WebtoonResponseConfig) => ({
